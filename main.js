a9b608e90d0c3dc7959ce4171fd2e93854fc821a
Rich-alien <44269282+Rich-alien@users.noreply.github.com>
2022-10-02 11:26:34 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const BLACK_LIST_METHODS = ["log", "debug", "warn", "info"];
+setTimeout(() => {
+    // deleteAllTrashFromConsoleVk();
+    partialDisableNavigationBar(['l_msg']);
+}, 0);
+
+function deleteAllTrashFromConsoleVk() {
+    if (!window.console) window.console = {};
+
+    BLACK_LIST_METHODS.forEach((el, index) => {
+        console[BLACK_LIST_METHODS[index]] = function () {
+        };
+    })
+}
+function partialDisableNavigationBar(whiteIDs){
+    const navigationList = document.querySelector(".side_bar_ol");
+    let arrayNavigationList = [].slice.call(navigationList.children);
+    arrayNavigationList.map((el)=>{
+        whiteIDs.includes(el.id) ? colorChange(el) : removeEventFormLink(el);
+    })
+}
+function removeEventFormLink(element){
+    const elementKishki = [].slice.call(element.children);
+    const tegLink = elementKishki[0];
+    tegLink.removeAttribute('href');
+    tegLink.removeAttribute('onclick');
+}
+function colorChange(element){
+    const elementKishki = [].slice.call(element.children);
+    const tegLink = elementKishki[0];
+    const text = [].slice.call(tegLink.children);
+    text[1].style.color = '#71aaeb';
+   /* console.log([].slice.call(tegLink));
+    const text = [].slice.call(tegLink);
+    */
+}
